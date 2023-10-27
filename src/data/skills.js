9472fdbd6341af616b69be750023e46b66c16b09
Pradhyumna-Joshi <pradhyumnajoshi07@gmail.com>
2023-10-27 22:06:00 +0530

    Intial Commit

---
+++ src/data/skills.js
@@ -0,0 +1,78 @@
+export const skills = [
+  {
+    name: "C",
+    image: " https://img.icons8.com/color/480/c-programming.png",
+  },
+  {
+    name: "C++",
+    image: " https://img.icons8.com/color/480/c-plus-plus-logo.png",
+  },
+  {
+    name: "React Js",
+    image: "https://img.icons8.com/color/480/react-native.png",
+  },
+  {
+    name: "Redux",
+    image:
+      "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+  },
+
+  {
+    name: "HTML",
+    image: "https://img.icons8.com/color/480/html-5--v1.png",
+  },
+  {
+    name: "CSS",
+    image: "https://img.icons8.com/color/480/css3.png",
+  },
+  {
+    name: "JavaScript",
+    image: "https://img.icons8.com/fluency/480/javascript.png",
+  },
+
+  {
+    name: "Node Js",
+    image: "https://img.icons8.com/fluency/480/node-js.png",
+  },
+
+  {
+    name: "MySQL",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+  },
+
+  {
+    name: "MongoDB",
+    image:
+      "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+  },
+  {
+    name: "Firebase",
+    image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+  },
+
+  {
+    name: "Java",
+    image: "https://img.icons8.com/color/480/java-coffee-cup-logo--v1.png",
+  },
+  {
+    name: "Kotlin",
+    image: "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+  },
+
+  {
+    name: "Android Studio",
+    image:
+      "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+  },
+  {
+    name: "Tailwind",
+    image: "https://img.icons8.com/color/480/tailwindcss.png",
+  },
+
+  {
+    name: "Git",
+    image:
+      "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+  },
+];
